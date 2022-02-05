44c05a5505481c8b337069a9cebf88379402a751
Patrick Garsow <patrick_garsow@hms.harvard.edu>
2022-02-05 16:41:15 +0000

    Practice App Scaffolding setup

--- practice/index.php
+++ practice/index.php
@@ -3,6 +3,13 @@
 
 <head>
     <title>Practice App</title>
+    <meta charset='utf-8'>
+    <link href=data: , rel=icon>
 </head>
+
+<body>
+    <h1>Practice App</h1>
+    <img src="images/hes-logo.png">
+</body>
 
 </html>
